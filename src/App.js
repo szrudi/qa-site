25feb3635b3236285c0c1893960f7398f53ae23e
Rudi Szabo <szrudi@gmail.com>
2021-02-22 19:02:48 +0100

    Move Provider back to index.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import { Header } from "./features/header/Header";
 import { InfoMessage } from "./features/info-message/InfoMessage";
 import { QuestionList } from "./features/question-list/QuestionList";
 import { QuestionForm } from "./features/question-form/QuestionForm";
-import { Provider } from "react-redux";
-import store from "./app/store";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Provider store={store}>
-        <InfoMessage />
-        <QuestionList />
-        <QuestionForm />
-      </Provider>
+      <InfoMessage />
+      <QuestionList />
+      <QuestionForm />
     </div>
   );
 }
